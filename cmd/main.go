0e68d6fd1de2ffc8ebf76c71d6713fbf9ef36261
abereg <bledbereq.gmail.com>
2024-06-26 09:17:17 +0700

    Реализовали логирование данных. написали функции для тестов

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"userServerAuth/internal/config"
 )
 
@@ -9,7 +11,42 @@ func main() {
 
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
-	// инициализировать логгер
+	log := setupLogger(cfg.Env)
+	log.Info("starting app",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPS.Port))
+	log.Debug("debage message")
+	log.Error("error message")
+	log.Warn("warn message")
+
+	// grpc сервер и хендлеры
 	// инициализировать точку входа в приложение
-	// запустить grpc сервер
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
