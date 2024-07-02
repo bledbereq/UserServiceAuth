a006f28da31b8e2c6053a9da7202a4e397d266ca
bledbereq <bledbereq@gmail.com>
2024-07-02 14:24:27 +0700

    Востановили ветку publickey

---
+++ cmd/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"UserServiceAuth/internal/app"
+	"UserServiceAuth/internal/config"
+	"flag"
+	"fmt"
+	"log/slog"
+
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	var configPath string
+	flag.StringVar(&configPath, "config", "", "path to config file")
+	flag.Parse()
+
+	// Валидация аргумента "config"
+	if configPath == "" {
+		fmt.Println("Usage: ./yourapp -config <path_to_config_file>")
+		os.Exit(1)
+	}
+
+	cfg := config.MustLoadByPath(configPath)
+	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting app",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port))
+
+	// Запуск gRPC сервера
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+	go application.GRPCSrv.Run()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	application.GRPCSrv.Stop()
+
+	log.Info("Gracefully stopped")
+}
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-required:"true"`
+	GRPC        GRPCconfig    `yaml:"grpc" env-required:"true"`
+}
+
+type GRPCconfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+type HttpServerConfig struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoadByPath(configPath string) *Config {
+	// Проверка наличия файла
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
+	}
+
+	return &cfg
+}
