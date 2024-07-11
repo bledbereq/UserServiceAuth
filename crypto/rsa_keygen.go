c70cb482317255337de8e5af611e3ee69a2005ca
bledbereq <bledbereq@gmail.com>
2024-07-11 16:38:46 +0700

    Вынесли функционал генерации ключей в отдельный файл

--- crypto/rsa_keygen.go
+++ crypto/rsa_keygen.go
@@ -1,58 +1,14 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
+	"UserServiceAuth/keygen"
 	"fmt"
-	"os"
 )
 
-func generateKeyPair() error {
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return err
-	}
-	privateFile, err := os.Create("gen/key/private.pem")
-	if err != nil {
-		return err
-	}
-	defer privateFile.Close()
-
-	privatePEM := &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
-	}
-	if err := pem.Encode(privateFile, privatePEM); err != nil {
-		return err
-	}
-
-	publicKey := &privateKey.PublicKey
-	publicFile, err := os.Create("gen/key/public.pem")
-	if err != nil {
-		return err
-	}
-	defer publicFile.Close()
-
-	publicPEM, err := x509.MarshalPKIXPublicKey(publicKey)
-	if err != nil {
-		return err
-	}
-
-	publicPEMBlock := &pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: publicPEM,
-	}
-	if err := pem.Encode(publicFile, publicPEMBlock); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func main() {
-	if err := generateKeyPair(); err != nil {
+	key_private_path := "../gen/key/private.pem"
+	key_public_path := "../gen/key/public.pem"
+	if err := keygen.GenerateKeyPair(key_private_path, key_public_path); err != nil {
 		fmt.Println("Error generating key pair:", err)
 		return
 	}
